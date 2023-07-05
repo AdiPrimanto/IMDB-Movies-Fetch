0391ee82ada7d068aedba914e58e51d93644613e
adiprimanto <adiprimanto.98@gmail.com>
2023-07-05 20:19:55 +0700

    fixbug service worker

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,19 @@
+// self.addEventListener("install", function (event) {
+//   event.waitUntil(
+//     caches.open("my-cache").then(function (cache) {
+//       return cache.addAll(["index.html", "style.css", "script.js"]);
+//     })
+//   );
+// });
+
+// self.addEventListener("fetch", function (event) {
+//   event.respondWith(
+//     caches.match(event.request).then(function (response) {
+//       return response || fetch(event.request);
+//     })
+//   );
+// });
+
 self.addEventListener("install", function (event) {
   event.waitUntil(
     caches.open("my-cache").then(function (cache) {
@@ -8,8 +24,18 @@ self.addEventListener("install", function (event) {
 
 self.addEventListener("fetch", function (event) {
   event.respondWith(
-    caches.match(event.request).then(function (response) {
-      return response || fetch(event.request);
-    })
+    caches
+      .match(event.request)
+      .then(function (response) {
+        // Mengembalikan respons dari cache jika ada
+        if (response) {
+          return response;
+        }
+        // Melakukan permintaan jaringan jika tidak ada respons dari cache
+        return fetch(event.request);
+      })
+      .catch(function (error) {
+        console.error("Gagal memuat:", error);
+      })
   );
 });
