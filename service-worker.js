cf0f2314408d75189eb98a299fd3d23385c2cb05
adiprimanto <adiprimanto.98@gmail.com>
2023-07-07 22:34:06 +0700

    add offline.html

--- service-worker.js
+++ service-worker.js
@@ -1,41 +1,71 @@
-// self.addEventListener("install", function (event) {
-//   event.waitUntil(
-//     caches.open("my-cache").then(function (cache) {
-//       return cache.addAll(["index.html", "style.css", "script.js"]);
-//     })
-//   );
-// });
-
-// self.addEventListener("fetch", function (event) {
-//   event.respondWith(
-//     caches.match(event.request).then(function (response) {
-//       return response || fetch(event.request);
-//     })
-//   );
-// });
-
 self.addEventListener("install", function (event) {
   event.waitUntil(
     caches.open("my-cache").then(function (cache) {
-      return cache.addAll(["index.html", "style.css", "script.js"]);
+      return cache.addAll([
+        "index.html",
+        "style.css",
+        "script.js",
+        "offline.html",
+        "https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css",
+        "https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js",
+        "https://code.jquery.com/jquery-3.5.1.min.js",
+        "https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js",
+      ]);
     })
   );
 });
 
 self.addEventListener("fetch", function (event) {
-  event.respondWith(
-    caches
-      .match(event.request)
-      .then(function (response) {
-        // Mengembalikan respons dari cache jika ada
-        if (response) {
-          return response;
-        }
-        // Melakukan permintaan jaringan jika tidak ada respons dari cache
-        return fetch(event.request);
-      })
-      .catch(function (error) {
-        console.error("Gagal memuat:", error);
-      })
-  );
+  // event.respondWith(
+  //   caches
+  //     .match(event.request)
+  //     .then(function (response) {
+  //       if (response) {
+  //         return response;
+  //       }
+
+  //       // Menjalankan permintaan jaringan hanya jika ada koneksi internet
+  //       if (!navigator.onLine) {
+  //         return caches.match("offline.html");
+  //       }
+
+  //       return fetch(event.request).then(function (response) {
+  //         let responseToCache = response.clone();
+  //         caches.open("my-cache").then(function (cache) {
+  //           cache.put(event.request, responseToCache);
+  //         });
+  //         return response;
+  //       });
+  //     })
+  //     .catch(function () {
+  //       return caches.match("offline.html"); // Mengembalikan halaman offline jika terjadi kesalahan lain
+  //     })
+  // );
+  // Mengecek apakah permintaan berasal dari domain eksternal
+  if (event.request.url.startsWith(self.location.origin)) {
+    event.respondWith(
+      caches
+        .match(event.request)
+        .then(function (response) {
+          if (response) {
+            return response;
+          }
+
+          if (!navigator.onLine) {
+            return caches.match("offline.html");
+          }
+
+          return fetch(event.request).then(function (response) {
+            let responseToCache = response.clone();
+            caches.open("my-cache").then(function (cache) {
+              cache.put(event.request, responseToCache);
+            });
+            return response;
+          });
+        })
+        .catch(function () {
+          return caches.match("offline.html");
+        })
+    );
+  }
 });
